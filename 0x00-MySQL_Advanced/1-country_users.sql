1b9d6669f0b18cb65077b1134de4790f84e88ce9
EYATUPE <eyatuphilipesamu@gmail.com>
2023-07-13 20:46:08 +0000

    Add enumeration of countries in the table

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,13 @@
+-- A SQL script that creates a table named users following these requirements:
+-- id, email, name, country(enumeration of US, CO and TN)
+
+CREATE TABLE IF NOT EXISTS users (
+	-- User's Unique identifier
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	-- User's Email address (maximum 255 characters)
+	email varchar(255) NOT NULL UNIQUE,
+	-- User's Name (maximum 255 characters)
+	name varchar(255)
+	-- Enumeration of selected countries 
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+)
